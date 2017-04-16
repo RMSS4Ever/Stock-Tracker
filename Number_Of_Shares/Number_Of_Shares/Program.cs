59ee69f7c0d89064ab116cb4f9ea15fb897cb877
RMSS4Ever <bscarlett2008@yahoo.com>
2017-04-15 18:02:54 -0700

    Method with Parameters/Number of Shares to purchase

---
+++ Number_Of_Shares/Number_Of_Shares/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Of_Shares
+{
+    class Program
+    {
+        
+        static double numberOfShares(double Bank, double StockPrice)
+        {
+            double result = Bank / StockPrice;
+
+            return result;
+
+        }
+
+        static void Main(string[] args)
+        {
+            // Variables
+            double Bank = 400.00;
+
+            WriteLine("Stock Price: ");
+            double stockPrice = Convert.ToDouble(ReadLine());
+
+            double nmbrShares = numberOfShares(Bank, stockPrice);
+
+            WriteLine("Amount of Shares to purchase: " + nmbrShares);
+
+            ReadLine();
+
+
+        }
+    }
+}
